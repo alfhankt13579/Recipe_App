19410c86773d921f180d1b9350dd351036f27977
alfhankt13579 <137772873+alfhankt13579@users.noreply.github.com>
2023-08-21 04:28:31 +0530

    Add files via upload

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -10,25 +10,25 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getAllRecipes(): Observable<recipeList[]> {
-    return this.http.get<recipeList[]>('http://localhost:3000/recipes');
+    return this.http.get<recipeList[]>('https://localhost:3000/recipes');
   }
 
   viewRecipe(id: number): Observable<recipeList> {
-    return this.http.get<recipeList>(`http://localhost:3000/recipes/${id}`);
+    return this.http.get<recipeList>(`https://localhost:3000/recipes/${id}`);
   }
 
   addRecipe(recipe: recipeList): Observable<recipeList> {
-    return this.http.post<recipeList>(`http://localhost:3000/recipes`, recipe);
+    return this.http.post<recipeList>(`https://localhost:3000/recipes`, recipe);
   }
 
   updateRecipe(id: number, recipe: recipeList): Observable<recipeList> {
     return this.http.put<recipeList>(
-      `http://localhost:3000/recipes/${id}`,
+      `https://localhost:3000/recipes/${id}`,
       recipe
     );
   }
 
   deleteRecipe(id: number): Observable<{}> {
-    return this.http.delete(`http://localhost:3000/recipes/${id}`);
+    return this.http.delete(`https://localhost:3000/recipes/${id}`);
   }
 }
